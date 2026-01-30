02caa3b8db020206438d2dedb7b2c5d7b147d184
Alexander <131615487+yar101@users.noreply.github.com>
2026-01-30 03:40:56 +0300

    feat:implement element-plus lib (#4)
    Co-authored-by: yar101 <alexander.yakimets@mail.ru>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import '../css/app.css';
 import './bootstrap';
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
