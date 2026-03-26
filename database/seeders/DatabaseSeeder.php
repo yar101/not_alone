0c58b48cc8a0f2e4bb4ed055245daacb86f0566b
yar101 <blackgn13@gmail.com>
2026-03-26 14:00:34 +0300

    feat: модель News, миграции и сидер с цветами категорий

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             ServiceCategorySeeder::class,
             ServiceTimeUnitSeeder::class,
             BanReasonSeeder::class,
+            NewsSeeder::class,
         ]);
     }
 }
