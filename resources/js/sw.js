fd97bc4fe5cde380c2e9dc2794775864256688f9
yar101 <blackgn13@gmail.com>
2026-05-09 19:08:00 +0300

    Оптимизация кэширования PWA: запрет кэша для sw.js, агрессивный кэш для ассетов и мгновенная активация воркера

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -41,3 +41,11 @@ self.addEventListener('notificationclick', (event) => {
         })
     )
 })
+
+self.addEventListener('install', () => {
+    self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim())
+})
