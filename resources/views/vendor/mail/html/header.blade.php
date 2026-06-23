75380fca94adf9ea546cbd32ee77a71ceb50518d
yar101 <blackgn13@gmail.com>
2026-06-23 20:21:28 +0300

    style: replace laravel email layout with custom brand layout
    - Use Not Alone logo in email headers
    - Replace standard copyright text with a friendly closing in footer

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
+<img src="{{ config('app.url') }}/app-logo-v3.png" class="logo" alt="Not Alone" style="height: 40px; width: auto;">
 </a>
 </td>
 </tr>
--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
+С уважением,<br>сервис Not Alone
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
