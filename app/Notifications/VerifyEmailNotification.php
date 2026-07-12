2e6047559d7d646f7ead9f1b2f53d7093e2c18e0
yar101 <blackgn13@gmail.com>
2026-07-12 13:03:54 +0300

    Fix: Add missing Queueable trait to VerifyEmailNotification

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -5,9 +5,11 @@ namespace App\Notifications;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Bus\Queueable;
 
 class VerifyEmailNotification extends VerifyEmail implements ShouldQueue
 {
+    use Queueable;
     public function toMail($notifiable): MailMessage
     {
         $url = $this->verificationUrl($notifiable);
